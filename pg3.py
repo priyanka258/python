09bd44fe7796c835a9865eaf68902cd5e2cbcc93
priyanka258 <51222551+priyanka258@users.noreply.github.com>
2019-07-03 15:26:22 +0530

    Update pg3.py

--- pg3.py
+++ pg3.py
@@ -1,5 +1,5 @@
 var=input()
-if(var=='a' || var=='e' || var=='i' || var=='o' || var=='u' || var=='A' || var=='E' || var=='I' || var=='O' || var=='U'):
+if(var=='a' or var=='e' or var=='i' or var=='o' or var=='u' or var=='A' or var=='E' or var=='I' or var=='O' or var=='U'):
   print("Vowel")
 elif(var="A" to var="Z"):
   print("Consonant")
