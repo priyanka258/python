546dbab5dd68c825de06e84d53697a057b80b789
priyanka258 <51222551+priyanka258@users.noreply.github.com>
2019-07-03 15:23:29 +0530

    Create pg3.py

---
+++ pg3.py
@@ -0,0 +1,7 @@
+var=input()
+if(var=='a' || var=='e' || var=='i' || var=='o' || var=='u' || var=='A' || var=='E' || var=='I' || var=='O' || var=='U'):
+  print("Vowel")
+elif(var="A" to var="Z"):
+  print("Consonant")
+else:
+  print("invalid")
